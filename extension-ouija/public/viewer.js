663355d5a34c7e0b9523ea98d44672f260dee260
David Cao <david@cao.st>
2019-09-28 15:40:27 -0700

    finish up frontend

--- extension-ouija/public/viewer.js
+++ extension-ouija/public/viewer.js
@@ -55,13 +55,22 @@ function logSuccess(hex, status) {
 }
 
 function drawViz() {
-  const margin = {left: 4, right: 4, top: 4, bottom: 4};
+  // TODO TEMP
+  const testData = [
+    {'letter': 'D', 'count': 1111},
+    {'letter': 'B', 'count': 2313},
+    {'letter': 'C', 'count': 2313},
+    {'letter': 'A', 'count': 3512},
+    {'letter': 'E', 'count': 5512},
+  ];
+
+  const margin = {left: 4, right: 8, top: 4, bottom: 4};
   const width = 318 - margin.left - margin.right;
   // Full height is 496
-  const height = 300 - margin.top - margin.bottom;
+  const height = 184 - margin.top - margin.bottom;
 
-  var xAxis = d3.scaleBand().range([0, width]).padding(0.1);
-  var yAxis = d3.scaleLinear().range([height, 0]);
+  var xAxis = d3.scaleLinear().range([0, width]);
+  var yAxis = d3.scaleBand().range([height, 0]).padding(0.1);
 
   var svg = d3.select(".viz").append("svg")
       .attr("width", width + margin.left + margin.right)
@@ -70,10 +79,59 @@ function drawViz() {
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   function update(data) {
-    const trans = d3.transition().duration(500);
+    const t = d3.transition().duration(500);
+    const bar = svg.selectAll("g").data(data);
+
+    xAxis.domain([0, d3.max(data, d => d.count)]);
+    yAxis.domain(data.map(d => d.letter));
+
+    // EXIT
+    bar.exit().transition(t).remove()
+
+    // UPDATE
+    bar.transition(t)
+      .attr("transform", (d) => `translate(${xAxis(d.count)}, ${yAxis(d.letter)})`)
+
+    bar.select("rect").transition(t)
+      .attr("width", d => xAxis(d.count))
+
+    bar.select("text").transition(t)
+      .tween("text", d => {
+        const v0 = this.textContent || "0";
+        const v1 = d.count;
+        const i = d3.interpolateRound(v0, v1);
+        return t => this.textContent = i(t);
+      });
+
+    // ENTER
+    const barEnter = bar
+      .enter().append("g")
+        .attr("transform", (d) => `translate(${margin.left}, ${height - margin.bottom})`)
+
+    barEnter.transition(t)
+      .attr("transform", d => `translate(${margin.left}, ${yAxis(d.letter)})`);
+
+    const rect = barEnter.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("height", yAxis.bandwidth())
+      .style("fill", "steelblue")
+      .attr("width", 0);
+
+    rect.transition(t).attr("width", d => xAxis(d.count));
+
+    const text = barEnter.append("text")
+      .text(d => `${d.letter}: ${d.count}`)
+      .attr("text-anchor", "right")
+      .style("font-size", "10px")
+      .style("fill", "white")
+      .attr("dx", d => xAxis(d.count) - 40)
+      .attr("dy", yAxis.bandwidth() / 2 + 2.5);
   }
+
+  update(testData);
 }
 
 $(function () {
-
+  drawViz();
 });
