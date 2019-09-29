442aab85155a7252d9a8a8353a87e58d64e40c66
David Cao <david@cao.st>
2019-09-29 06:59:14 -0700

    we out here bois

--- extension-ouija/public/viewer.js
+++ extension-ouija/public/viewer.js
@@ -69,7 +69,7 @@ function logSuccess(hex, status) {
   twitch.rig.log('EBS request returned '+hex+' ('+status+')');
 }
 
-var socket = io("https://twitch-ouija.herokuapp.com:80");
+var socket = io("http://twitch-ouija.herokuapp.com:80");
 
 function votingFinished() {
   disableVoting();
